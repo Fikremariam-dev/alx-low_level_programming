70fa8f869af802fbec9e1c4c4442c6234bab3748
root <hdreseden@gmail.com>
2022-03-11 11:29:45 -0800

    q8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int z;
-
-for (z = 48; z <= 57; z++)
-{
-putchar(z);
-}
-for (z = 97; z <= 102;z++)
-{
-putchar(z)
-}
-putchar('\n');
-return (0);
+	int z;
+	
+	for (z = 48; z <= 57; z++)
+	{
+		putchar(z);
+	}
+	for (z = 97; z <= 102;z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
 }
