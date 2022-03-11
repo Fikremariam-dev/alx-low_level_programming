732a8534a881d487eb1ea953d436ac16bc10b0e0
root <hdreseden@gmail.com>
2022-03-11 11:24:34 -0800

    fixed q4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,22 +6,15 @@
  */
 int main(void)
 {
-int y;
-
-for (y = 97; y <= 122; y++)
-{
-<<<<<<< HEAD
-       if (m == 101 || m == 113)
-=======
-if (y == 101 || y == 113)
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-{
-}
-else
-{
-putchar(y);
-}
-}
-putchar('\n');
-return (0);
+	int y;
+	
+	for (y = 97; y <= 122; y++)
+	{
+		if (y != 101 && y != 113)
+		{
+			putchar(y);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
